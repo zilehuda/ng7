21442a026074797cd0f016b6b8bcb27f351fbeed
zilehuda <zilehuda.tariq@gmail.com>
2019-02-12 00:30:16 +0500

    prod to heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/your_app_name'));
+app.use(express.static('./dist/ng7'));
 
 app.get('/*', function(req,res) {
     
